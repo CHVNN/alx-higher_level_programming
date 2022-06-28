41051246d32fa6078a0fb2cd776f6a817bb6af99
CHVNN <iwuohaustin95@gmail.com>
2022-06-28 23:29:15 +0000

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 num_combo = 0
 while num_combo <= 89:
     if num_combo % 10 == 0:
-        num_combo += 1 + number // 10
+        num_combo += 1 + num_combo // 10
     print("{:02d}".format(num_combo), end='\n' if num_combo == 89 else ", ")
     num_combo += 1
