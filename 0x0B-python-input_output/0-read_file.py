eb204c00c741e10b2b37aa68b3157bfedada49fe
CHVNN <iwuohaustin95@gmail.com>
2022-08-04 01:16:46 +0000

    0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''Defines a function called read_file
+'''
+
+
+def read_file(filename=""):
+    '''Reads a file and prints it out
+    Args:
+       filename (dir): The path to the file being read
+    Return: none
+    '''
+    with open(filename) as f:
+        for line in f:
+            print(line, end="")
